2982b87bc9b1e57c56f9f4bad5e0245dac0857d8
Maithali <ghardem1@gmail.com>
2025-01-26 15:41:59 +0530

    setup complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const index = () => {
   return (
     <div>index
-       <h1 className="text-3xl font-bold underline">      Hello world!    </h1>
+       <h1 className="text-3xl font-bold underline">      Hello world!    </h1>hardhat
     </div>
   )
 }
