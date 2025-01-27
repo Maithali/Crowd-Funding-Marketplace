44eeb2b92668d0f172bf40854a070bf514031fce
Maithali <ghardem1@gmail.com>
2025-01-27 13:45:09 +0530

    file adding

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+// Internal IMPORT
+import { NavBar, Footer } from "../Components";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return
+  <>
+    <NavBar/>
+     <Component {...pageProps} />;
+    <Footer/>
+  </>
+ 
 }
